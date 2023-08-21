bafe7e8393231ac0a3a2e052f163d9a80b8429ec
David Jose <vikruzdavid@gmail.com>
2023-08-21 12:02:26 -0500

    Initial Commit

--- web/app/app.py
+++ web/app/app.py
@@ -13,18 +13,11 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import os
-import socket
-
 from flask import Flask
-from redis import Redis
-
 
 app = Flask(__name__)
-redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=6379)
 
 
 @app.route('/')
 def hello():
-    redis.incr('hits')
-    return 'Hello Container World! I have been seen %s times and my hostname is %s.\n' % (redis.get('hits'),socket.gethostname())
+    return 'Hello Container World!'
