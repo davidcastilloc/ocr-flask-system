47f46b97386205aa0fef04a757fb700f98c7a886
David Jose <vikruzdavid@gmail.com>
2023-08-21 22:45:31 -0500

    Clean words

--- web/app/app.py
+++ web/app/app.py
@@ -1,26 +1,15 @@
-# Copyright 2018 Cisco Systems, Inc.
-# All rights reserved.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
-
 from flask import Flask
 from flask import render_template
 
-from .controllers.ocr import extract_text_from_recipe
+from .controllers.ocr import get_recipe_list_items
 
 app = Flask(__name__)
 
+img_recipe = "./app/static/img/text2.jpg"
+
 @app.route('/')
 def index():
-    list_detected_words = extract_text_from_recipe(route_img_recipe="./app/static/img/text.png")
-    return render_template('index.html.jinja', titulo="OCR Flask", recipe=list_detected_words)
+    return render_template('index.html.jinja',
+                           titulo="OCR Flask",
+                           recipe=get_recipe_list_items(img_recipe),
+                           img_example = img_recipe.split("app/")[-1])
--- web/app/controllers/ocr.py
+++ web/app/controllers/ocr.py
@@ -1,6 +1,59 @@
 import pytesseract
-def extract_text_from_recipe(route_img_recipe):
+from cleantext import clean
+import string
+
+
+def extract_raw_text_from_recipe(route_img_recipe):
     custom_config = r'--oem 3 --psm 6 --user-words "spa.user-words"'
-    items = pytesseract.image_to_string(route_img_recipe, lang='spa', config=custom_config).lower().split(" ")
-    items_filtered = [item for item in items if len(item) > 8]
-    return items_filtered
+    raw_text = pytesseract.image_to_string(
+        route_img_recipe, lang='spa', config=custom_config)
+    return raw_text
+
+
+def clean_raw_text(raw_text):
+    cleaned_text = clean(raw_text, fix_unicode=True,
+                         to_ascii=True,
+                         lower=True,
+                         no_line_breaks=False,
+                         no_urls=True,
+                         no_emails=True,
+                         no_phone_numbers=True,
+                         no_numbers=True,
+                         no_digits=True,
+                         no_currency_symbols=False,
+                         no_punct=True,
+                         replace_with_punct="",
+                         replace_with_url="",
+                         replace_with_email="",
+                         replace_with_phone_number="",
+                         replace_with_number="",
+                         replace_with_digit="",
+                         replace_with_currency_symbol="<CUR>",
+                         lang="en")
+    return cleaned_text
+
+
+def extract_words_from_cleaned_text_and_return_list(cleaned_text):
+    return [word.strip(string.punctuation) for word in cleaned_text.split() if word.strip(string.punctuation).isalnum() and len(word) > 9]
+
+
+def remove_duplicates_on_list(list_words):
+    return list(set(list_words))
+
+
+def read_glosary_and_return_list(route_glosary='code/app/spa.user-words'):
+    with open(route_glosary) as f:
+        glosary = f.read()
+    return glosary.split("\n")
+
+
+def return_word_if_matches_on_glosary(word, glosary):
+    return list(filter(lambda a: word in a, glosary))[-1]  # return last match
+
+
+def get_recipe_list_items(route_uploaded_img_recipe="./app/static/img/text.png"):
+    raw_text = extract_raw_text_from_recipe(route_uploaded_img_recipe)
+    cleaned_text = clean_raw_text(raw_text)
+    words_extracted = extract_words_from_cleaned_text_and_return_list(
+        cleaned_text)
+    return remove_duplicates_on_list(words_extracted)
