834fd0f63bbe59bd687c1f356ee3037f64e84937
David Jose <vikruzdavid@gmail.com>
2023-09-30 09:22:41 -0500

    Refactor Models

--- compumedic/models/CompareFeatureModels.py
+++ compumedic/models/CompareFeatureModels.py
@@ -2,7 +2,7 @@ from compumedic.extensions import db
 
 
 class Product(db.Model):
-    __tablename__ = 'product'
+    __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     description = db.Column(db.String(120))
@@ -12,7 +12,7 @@ class Product(db.Model):
 
 
 class Store(db.Model):
-    __tablename__ = 'store'
+    __tablename__ = 'stores'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     description = db.Column(db.String(120), )
