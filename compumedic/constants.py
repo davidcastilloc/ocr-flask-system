360f334c129e1e04d1303b6f4559fd1bd86de36c
David Jose <vikruzdavid@gmail.com>
2023-09-29 19:14:38 -0500

    Config Redis updated

--- compumedic/constants.py
+++ compumedic/constants.py
@@ -1,5 +1,4 @@
-CERT_PEM=/etc/letsencrypt/live/compymedic.duckdns.org/cert.pem
-CERT_CHAIN=/etc/letsencrypt/live/compymedic.duckdns.org/chain.pem
-CERT_FULLCHAIN=/etc/letsencrypt/live/compymedic.duckdns.org/fullchain.pem
-CERT_PRIVKEY=/etc/letsencrypt/live/compymedic.duckdns.org/privkey.pem
-
+CERT_PEM = "/etc/letsencrypt/live/compymedic.duckdns.org/cert.pem"
+CERT_CHAIN = "/etc/letsencrypt/live/compymedic.duckdns.org/chain.pem"
+CERT_FULL_CHAIN = "/etc/letsencrypt/live/compymedic.duckdns.org/fullchain.pem"
+CERT_PRIVATE_KEY = "/etc/letsencrypt/live/compymedic.duckdns.org/privkey.pem"
