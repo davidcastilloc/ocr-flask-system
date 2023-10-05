b03e5431c5535079a4c523849a9a16c7a2f0125a
David Jose <vikruzdavid@gmail.com>
2023-10-05 16:34:00 -0500

    Si no se consigue el producto no se inserta en el diccionario

--- compumedic/services/CompareFeatureService.py
+++ compumedic/services/CompareFeatureService.py
@@ -1,12 +1,9 @@
-from compumedic.cache import cache
-from compumedic.models.CompareFeatureModels import get_store_by_id
 from compumedic.services.scrapping.BoticasHogarYSaludQueryImp import BoticasHogarYSalud
 from compumedic.services.scrapping.BoticasPeruScrapperQueryImp import BoticasPeruScrapperQueryImp
 from compumedic.services.scrapping.BoticasYSaludQueryImp import BoticasYSaludQueryImp
 from compumedic.services.scrapping.FarmaciaUniversalQueryImp import FarmaciaUniversalQueryImp
 from compumedic.services.scrapping.InkaFarmaQueryImp import InkaFarmaQueryImp
 from compumedic.services.scrapping.MiFarmaQueryImp import MiFarmaQueryImp
-
 
 def get_data(query):
     inst_bp = BoticasPeruScrapperQueryImp(query=query)
@@ -16,42 +13,27 @@ def get_data(query):
     inst_ik = InkaFarmaQueryImp(query=query)
     inst_bs = BoticasYSaludQueryImp(query=query)
 
-    inst_bp.execute_query()
-    inst_fu.execute_query()
-    inst_bhs.execute_query()
-    inst_mf.execute_query()
-    inst_ik.execute_query()
-    inst_bs.execute_query()
+    data = []
 
-    return [
-        {
-            'name': "MiFarma",
-            'photo': "Mifarma.png",
-            'product': inst_mf.get_result()
-        },
-        {
-            'name': "BoticasHogarYSalud",
-            'photo': "BoticasHogarYSalud.png",
-            'product': inst_bhs.get_result()
-        },
-        {
-            'name': "InkaFarma",
-            'photo': "Inkafarma.png",
-            'product': inst_ik.get_result()
-        },
-        {
-            'name': "BoticasYSalud",
-            'photo': "BoticasYSalud.png",
-            'product': inst_bs.get_result()
-        },
-        {
-            'name': "FarmaciaUniversal",
-            'photo': "FarmaciaUniversal.png",
-            'product': inst_fu.get_result()
-        },
-        {
-            'name': "BoticasPeru",
-            'photo': "BoticasPeru.png",
-            'product': inst_bp.get_result()
-        }
-    ]
+    # Define una función para ejecutar la consulta y agregar el resultado al diccionario
+    def execute_and_append_result(inst, name, photo, data):
+        try:
+            inst.execute_query()
+            data.append({
+                'name': name,
+                'photo': photo,
+                'product': inst.get_result()
+            })
+        except Exception as e:
+            # Puedes manejar la excepción aquí si lo necesitas
+            print(f"Error al ejecutar {name}: {str(e)}")
+
+    # Ejecuta y agrega los resultados al diccionario
+    execute_and_append_result(inst_mf, "MiFarma", "Mifarma.png", data)
+    execute_and_append_result(inst_bhs, "BoticasHogarYSalud", "BoticasHogarYSalud.png", data)
+    execute_and_append_result(inst_ik, "InkaFarma", "Inkafarma.png", data)
+    execute_and_append_result(inst_bs, "BoticasYSalud", "BoticasYSalud.png", data)
+    execute_and_append_result(inst_fu, "FarmaciaUniversal", "FarmaciaUniversal.png", data)
+    execute_and_append_result(inst_bp, "BoticasPeru", "BoticasPeru.png", data)
+
+    return data
