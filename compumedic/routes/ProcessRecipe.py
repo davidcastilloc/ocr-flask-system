708a30561731a11fec1da86ef2478faced27ba8d
David Jose <vikruzdavid@gmail.com>
2023-10-07 21:50:50 -0500

    Refactor Services

--- compumedic/routes/ProcessRecipe.py
+++ compumedic/routes/ProcessRecipe.py
@@ -1,26 +1,33 @@
 import os.path
-
 from flask import Blueprint, request, render_template
 
 from compumedic.services.RecipeProcessorService import RecipeProcessor
-# Logger
+# Import Logger
 from compumedic.utils.Logger import Logger
 
 upload = Blueprint('process_recipe_blueprint', __name__, static_folder='../static')
 
+# Create a logger instance
+logger = Logger()
 
 @upload.post('/')
 def process():
     """
     Display the upload form and handle file upload.
     """
-    ocr_inst = RecipeProcessor()
-    uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        uploaded_file.save(os.path.join(upload.static_folder, "uploads/") + uploaded_file.filename)
-        return render_template('recipe.jinja2',
-                               titulo="OCR Flask",
-                               lists=ocr_inst.process_recipe(uploaded_file.filename),
-                               img_filename=uploaded_file.filename)
-    else:
-        return render_template('upload_recipe.jinja2')
+    try:
+        ocr_inst = RecipeProcessor()
+        uploaded_file = request.files['file']
+
+        if uploaded_file.filename != '':
+            uploaded_file.save(os.path.join(upload.static_folder, "uploads/") + uploaded_file.filename)
+            return render_template('recipe.jinja2',
+                                   lists=ocr_inst.process_recipe(uploaded_file.filename),
+                                   img_filename=uploaded_file.filename)
+        else:
+            return render_template('upload_recipe.jinja2')
+    except Exception as e:
+        # Log the exception
+        logger.error(f"An error occurred: {str(e)}")
+        # Handle the error or return an appropriate response
+        return render_template('error_template.jinja2', error_message="An error occurred while processing the request.")
--- compumedic/services/CompareFeatureService.py
+++ compumedic/services/CompareFeatureService.py
@@ -1,41 +1,40 @@
-from compumedic.services.scrapping.BoticasHogarYSaludQueryImp import BoticasHogarYSalud
-from compumedic.services.scrapping.BoticasPeruScrapperQueryImp import BoticasPeruScrapperQueryImp
-from compumedic.services.scrapping.BoticasYSaludQueryImp import BoticasYSaludQueryImp
-from compumedic.services.scrapping.FarmaciaUniversalQueryImp import FarmaciaUniversalQueryImp
-from compumedic.services.scrapping.InkaFarmaQueryImp import InkaFarmaQueryImp
-from compumedic.services.scrapping.MiFarmaQueryImp import MiFarmaQueryImp
 from compumedic.utils.Logger import Logger
-import traceback
+from compumedic.services.scrapping.BoticasHogarYSaludQueryImp import BoticasHogarYSalud
+from compumedic.services.scrapping.BoticasYSaludQueryImp import BoticasYSaludQueryImp
+from compumedic.services.scrapping.MiFarmaQueryImp import MiFarmaQueryImp
+from compumedic.services.scrapping.InkaFarmaQueryImp import InkaFarmaQueryImp
+from compumedic.services.scrapping.FarmaciaUniversalQueryImp import FarmaciaUniversalQueryImp
+from compumedic.services.scrapping.BoticasPeruScrapperQueryImp import BoticasPeruScrapperQueryImp
+class Scraper:
+    def __init__(self, query):
+        self.query = query
+        self.scrapers = [
+            (BoticasHogarYSalud(self.query)),
+            (BoticasYSaludQueryImp(self.query)),
+            (MiFarmaQueryImp(self.query)),
+            (InkaFarmaQueryImp(self.query)),
+            (FarmaciaUniversalQueryImp(self.query)),
+            (BoticasPeruScrapperQueryImp(self.query)),
+        ]
 
-def get_data(query)->list:
-    inst_bp = BoticasPeruScrapperQueryImp(query=query)
-    inst_fu = FarmaciaUniversalQueryImp(query=query)
-    inst_bhs = BoticasHogarYSalud(query=query)
-    inst_mf = MiFarmaQueryImp(query=query)
-    inst_ik = InkaFarmaQueryImp(query=query)
-    inst_bs = BoticasYSaludQueryImp(query=query)
-    data = []
-    # Define una función para ejecutar la consulta y agregar el resultado al diccionario
-    def execute_query(inst, name, photo, data):
+    def execute_and_log(self, scraper, name, photo, data):
         try:
-            inst.execute_query()
+            scraper.execute_query()
             data.append({
                 'name': name,
                 'photo': photo,
-                'product': inst.get_result().to_dict()
+                'product': scraper.get_result().to_dict()
             })
         except IndexError as e:
-              print(f"ERROR: EN INSTANCIA : {inst} No se encuentra el producto..{e}")
+            print(f"QueryException: {name} No se encuentra el producto.{e}")
+            Logger.add_to_log(f"QueryException: {name} No se encuentra el producto.{e}", str(e))
         except Exception as e:
-            # Puedes manejar la excepción aquí si lo necesitas
-            Logger.add_to_log("error", str(e))
-            Logger.add_to_log("error", traceback.format_exc())
+            Logger.add_to_log("QueryException Rare: ", str(e))
 
-    #Ejecuta y agrega los resultados al diccionario
-    execute_query(inst_bs, "BoticasYSalud", "BoticasYSalud.png", data)
-    execute_query(inst_mf, "MiFarma", "Mifarma.png", data)
-    execute_query(inst_bhs, "BoticasHogarYSalud", "BoticasHogarYSalud.png", data)
-    execute_query(inst_ik, "InkaFarma", "Inkafarma.png", data)
-    execute_query(inst_fu, "FarmaciaUniversal", "FarmaciaUniversal.png", data)
-    execute_query(inst_bp, "BoticasPeru", "BoticasPeru.png", data)
-    return sorted(data, key=lambda x: x['product'].get('price'))
+    def get_data(self):
+        data = []
+
+        for scraper in self.scrapers:
+            self.execute_and_log(scraper, scraper.STORE_NAME, scraper.STORE_LOGO, data)
+
+        return sorted(data, key=lambda x: x['product'].get('price'))
